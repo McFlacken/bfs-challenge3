e42d4ca0ed91ab5c6416aec28019aeea8dc27247
McFlacken <angel.i.martinez@gmail.com>
2020-07-17 19:46:44 -0600

    add comments to index.js to make it easy to understand

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//this is a hard-coded variable that stores a few cat image urls
 var cats = ["https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1503431128871-cd250803fa41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1569591159212-b02ea8a9f239?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1547045662-e5a75e7238c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60", "https://images.unsplash.com/photo-1552933529-e359b2477252?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"]
 
+//this is the function that selects a random url from the array defined above and displays it
 function getCat(){
     var randomCat = cats[Math.floor(Math.random()*cats.length)];
     document.getElementById("gato").innerHTML = `
@@ -7,6 +9,8 @@ function getCat(){
         `;
 };
 
+//this calls the function so the first image is displayed when the page loads
 getCat();
 
+//this is the event listener that selects another random cat to display
 document.getElementById('show-a-cat').addEventListener('click', getCat);
